61715f0a25aaaf574fce0508e8971b7e405fa58f
Shuangu <wshuang661@gmail.com>
2024-11-29 22:28:53 -0500

    yay

--- HW11.ino
+++ HW11.ino
@@ -1,7 +1,6 @@
 int ledPins[] = {2, 3, 4, 5, 6, 7}; 
 int numLEDs = 6;                    
-int delayTime = 10000;              
-
+          
 void setup() {
   for (int i = 0; i < numLEDs; i++) {     
     pinMode(ledPins[i], OUTPUT);
@@ -11,14 +10,13 @@ void setup() {
 
 void loop() {
   // Turn on LEDs one by one
-  for (int i = 0; i < numLEDs; i++) { 
-    digitalWrite(ledPins[i], HIGH);       
-    delay(delayTime);                     
-  }
 
-  // Turn off all 
-  for (int i = 0; i < numLEDs; i++) { 
-    digitalWrite(ledPins[i], LOW);        
-  }
+float num=millis()/10000%6;
 
+for(int i=0;i<numLEDs;i++){
+  digitalWrite(ledPins[i], LOW); 
+}
+for(int i=0;i<num+1;i++){
+  digitalWrite(ledPins[i], HIGH); 
+}
 }
